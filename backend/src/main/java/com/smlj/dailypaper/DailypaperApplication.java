ae811cea31136c1d069f2041b2841059f72ac345
kaclok <572005238@qq.com>
2024-06-24 10:06:29 +0800

    提交

---
+++ backend/src/main/java/com/smlj/dailypaper/DailypaperApplication.java
@@ -0,0 +1,11 @@
+package com.smlj.dailypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailypaperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DailypaperApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/smlj/dailypaper/DailypaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smlj.dailypaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailypaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

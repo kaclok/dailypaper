47321ae32d2fa562fd58868453312b982906b242
kaclok <572005238@qq.com>
2024-06-27 16:56:35 +0800

    widget体系，类似unity的NGUI组件，可以参考element-plus的实现

---
+++ frontend/dailypaper/src/widget/DropDownList.vue
@@ -0,0 +1,16 @@
+<script setup>
+// 实现：（单选）
+// ref: element-plus
+// ref: https://cn.vuejs.org/guide/essentials/forms.html
+// 1、外部参数传递让其默认选中特定的选项，并且触发事件进行逻辑和UI刷新
+// 2、动态构建选项列表
+// 3、对于选中的 和 未选中的 选项有不同的css表现，比如放大
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/dailypaper/src/widget/Toggle.vue
@@ -0,0 +1,12 @@
+<script setup>
+// 实现：（多选一）
+// ref: element-plus
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>

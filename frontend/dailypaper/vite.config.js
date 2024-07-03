84a02c2eaea49b3357e533daa1f6a0dcb91363f3
kaclok <572005238@qq.com>
2024-07-03 15:02:00 +0800

    引入Element-Plus  https://element-plus.org/zh-CN/guide/quickstart.html

--- frontend/dailypaper/vite.config.js
+++ frontend/dailypaper/vite.config.js
@@ -3,10 +3,21 @@ import {defineConfig} from 'vite'
 // https://www.cnblogs.com/heavenYJJ/p/18058142
 import vue from '@vitejs/plugin-vue'
 
+// 按需自动导入Element-Plus https://element-plus.org/zh-CN/guide/quickstart.html
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
-export default defineConfig({
+defineConfig({
     plugins: [
         vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
     ],
     server: {
         hmr: true, // 开启热更新
@@ -18,3 +29,7 @@ export default defineConfig({
         }
     }
 })
+
+export default {
+    defineConfig,
+}

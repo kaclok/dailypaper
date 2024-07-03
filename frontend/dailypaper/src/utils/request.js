480423ad6252ee51f752114a4e470c9031d36de5
kaclok <572005238@qq.com>
2024-07-03 22:56:18 +0800

    导入axios

--- frontend/dailypaper/src/utils/request.js
+++ frontend/dailypaper/src/utils/request.js
@@ -1,15 +1,23 @@
 import axios from "axios"
 
-const baseURL = "http://localhost:8080";
-const axiosInstance = axios.create({baseURL});
+// https://www.axios-http.cn/docs/instance
+const springBootURL = "http://localhost:8089";
+// https://www.axios-http.cn/docs/config_defaults
+const axiosInstance = axios.create({baseURL: springBootURL});
 
+// https://www.axios-http.cn/docs/interceptors
 // 添加响应拦截器，其实是把异步成功回调、失败回调给统一封装
-axiosInstance.interceptors.request.use(
+axiosInstance.interceptors.response.use(
     success => {
+        // 2xx 范围内的状态码都会触发该函数, 对响应数据做点什么
         return success.data;
-    }, err => {
-        console.log(err);
+    }, fail => {
+        console.log(fail);
         // 异步状态转换为失败状态，走到catch分支
-        return Promise.reject(err);
+        return Promise.reject(fail);
     }
 )
+
+export {
+    axiosInstance,
+}
---
+++ frontend/dailypaper/src/utils/timeutil.js
@@ -0,0 +1,20 @@
+// 获取当前时间戳
+function nowTimestamp() {
+    return new Date().getTime() / 1000;
+}
+
+// 获取当前日期
+function nowDate() {
+    let date = new Date();
+    date.setHours(0, 0, 0, 0);
+}
+
+// "2021-05-08 12:50:30"转换为时间戳
+function toTimestamp(dateString) {
+    return new Date(dateString).getTime() / 1000;
+}
+
+export default {
+    nowTimestamp,
+    toTimestamp,
+}

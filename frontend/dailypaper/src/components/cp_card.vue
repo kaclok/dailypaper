9360923c957f9717d2bdd3e8a91519bbcd3f6501
kaclok <572005238@qq.com>
2024-07-10 16:56:43 +0800

    有人可能直接使用props.content去v-model，但是vue3禁止子组件修改父组件的props，所以会有readonly警告。

--- frontend/dailypaper/src/components/cp_card.vue
+++ frontend/dailypaper/src/components/cp_card.vue
@@ -23,12 +23,6 @@ function isToday() {
 function hasContent() {
     return props.content != null && props.content.trim() !== "";
 }
-
-onBeforeUpdate(() => {
-    // 父组件变更的时候，通知子组件的props, 但是子组件的v-model绑定的不是props, 所以需要在此更新
-    refTextContent.value = props.content;
-});
-
 </script>
 
 <template>
@@ -39,7 +33,7 @@ onBeforeUpdate(() => {
 
         <!-- 非空状态 -->
         <el-input v-if="(isToday() || (!isToday() && hasContent()))"
-                  v-model="refTextContent"
+                  v-model="props.content"
                   :rows="7"
                   type="textarea"
                   placeholder="输入日报内容"
@@ -54,7 +48,7 @@ onBeforeUpdate(() => {
         <el-empty v-if="(!isToday() && !hasContent())" style="width:280px; height:150px; z-index:3000;"
                   description="未填写内容"/>
 
-        <el-button @click="$emit('onEdit', props.id, refTextContent)" v-if="isToday()" type="success"
+        <el-button @click="$emit('onEdit', props.id, props.name)" v-if="isToday()" type="success"
                    circle :dark="true">提交
         </el-button>
     </el-card>

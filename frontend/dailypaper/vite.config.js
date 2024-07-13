c6e8830d2e25a6d1611005f13a44d4e558e5b60b
kaclok <572005238@qq.com>
2024-07-13 20:00:04 +0800

    笔误

--- frontend/dailypaper/vite.config.js
+++ frontend/dailypaper/vite.config.js
@@ -50,7 +50,7 @@ export default defineConfig((env) => {
                 // 猜测是服务器上的rewrite不生效
                 // 原因可能是： 1、服务器没有nodejs环境  2、服务器是linux,而开发环境是win  3、nginx管理web会有自己的cors策略
                 '/api': {
-                    target: 'http://localhost:8089', //config['VITE_BASE_API'],//这是你要跨域请求的地址前缀
+                    target: config['VITE_BASE_API'],//这是你要跨域请求的地址前缀
                     changeOrigin: true,//开启跨域
                     //去除前缀api
                     rewrite: (path) => path.replace(/^\/api/, '')

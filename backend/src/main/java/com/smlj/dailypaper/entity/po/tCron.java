4c11a9c1b5bb918b57abb9eb439d080889d40329
kaclok <572005238@qq.com>
2024-08-15 14:58:26 +0800

    linux的定时器任务

---
+++ backend/src/main/java/com/smlj/dailypaper/entity/po/tCron.java
@@ -0,0 +1,15 @@
+package com.smlj.dailypaper.entity.po;
+
+import lombok.Data;
+
+// https://blog.csdn.net/Alex_81D/article/details/85337730
+// https://cron.ciding.cc/
+// https://blog.csdn.net/qianlixiaomage/article/details/106599951
+// https://www.runoob.com/w3cnote/linux-crontab-tasks.html
+// @Data特性可以帮助自己写getter、setter等函数
+@Data
+public class tCron {
+    private int id;
+    private String desc;
+    private String cron;
+}

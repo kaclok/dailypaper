0241909c62770b980c46a9970e00494ecb75e9d0
kaclok <572005238@qq.com>
2024-11-16 15:01:07 +0800

    // https://www.axios-http.cn/docs/urlencoded 默认情况下，axios将 JavaScript 对象序列化为 JSON 。 要以application/x-www-form-urlencoded格式发送数据，您可以使用以下选项之一。 // 当请求头中的 content-type 是 application/x-www-form-urlencoded 时，Axios 将自动地将普通对象序列化成 urlencoded 的格式。
    // https://www.axios-http.cn/docs/multipart 从 v0.27.0 版本开始，当请求头中的 Content-Type 是 multipart/form-data 时，Axios 支持自动地将普通对象序列化成一个 FormData 对象

--- frontend/dailypaper/src/framework/services/net/NAxios.js
+++ frontend/dailypaper/src/framework/services/net/NAxios.js
@@ -1,5 +1,11 @@
 import axios from "axios"
 
+// https://www.axios-http.cn/docs/urlencoded 默认情况下，axios将 JavaScript 对象序列化为 JSON 。 要以application/x-www-form-urlencoded格式发送数据，您可以使用以下选项之一。
+// 当请求头中的 content-type 是 application/x-www-form-urlencoded 时，Axios 将自动地将普通对象序列化成 urlencoded 的格式。
+
+// https://www.axios-http.cn/docs/multipart 从 v0.27.0 版本开始，当请求头中的 Content-Type 是 multipart/form-data 时，Axios 支持自动地将普通对象序列化成一个 FormData 对象
+
+
 // https://www.axios-http.cn/docs/instance
 // https://www.axios-http.cn/docs/config_defaults
 const springBootURL = import.meta.env.VITE_BASE_API;

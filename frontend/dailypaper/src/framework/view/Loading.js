294fa93679ff5d2c2b0e73020f44627a26e47547
kaclok <572005238@qq.com>
2024-12-09 10:53:07 +0800

    十行代码实现全局loading  https://www.bilibili.com/video/BV13byLYLEVq?spm_id_from=333.788.videopod.sections&vd_source=5c9f5bd891aee351c325bcf632b5550f

---
+++ frontend/dailypaper/src/framework/view/Loading.js
@@ -0,0 +1,20 @@
+// https://www.bilibili.com/video/BV13byLYLEVq?spm_id_from=333.788.videopod.sections&vd_source=5c9f5bd891aee351c325bcf632b5550f
+// 全局loading
+import {ref, computed} from "vue"
+
+const _loadingCnt = ref(0);
+
+const loading = computed({
+    get() {
+        return _loadingCnt.value > 0;
+    },
+    set(value) {
+        _loadingCnt.value += (value ? 1 : -1);
+        _loadingCnt.value = Math.max(0, _loadingCnt.value);
+    }
+})
+
+export {
+    loading
+}
+

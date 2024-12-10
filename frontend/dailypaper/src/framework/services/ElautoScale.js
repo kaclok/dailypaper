ff50de4146001ec08a29dc8281242c95f8e78165
kaclok <572005238@qq.com>
2024-12-10 08:28:07 +0800

    elAutoScaleByWindow

--- frontend/dailypaper/src/framework/services/ElautoScale.js
+++ frontend/dailypaper/src/framework/services/ElautoScale.js
@@ -1,14 +1,14 @@
 // 大屏自适应解决方案
 // https://www.bilibili.com/video/BV1bT42117Fc?spm_id_from=333.788.videopod.sections&vd_source=5c9f5bd891aee351c325bcf632b5550f
 
-function elAutoScale(selector, options) {
+function elAutoScaleByWindow(selector, options, listen) {
     const el = document.getElementById(selector);
     const {width, height} = options;
 
     el.style.transformOrigin = "top left";
     el.style.transition = "transform 0.2s"
 
-    function init() {
+    function refresh() {
         const scaleX = innerWidth / width;
         const scaleY = innerHeight / height;
         // 获取宽高缩放最小值
@@ -20,12 +20,18 @@ function elAutoScale(selector, options) {
         el.style.transform = `translate(${left}px, ${top}px) scale(${scale})`;
     }
 
-    init()
+    refresh()
 
     // 监听窗口缩放事件
-    addEventListener('resize', init)
+    if (listen) {
+        addEventListener('resize', refresh)
+    } else {
+        removeEventListener('resize', refresh)
+    }
 
     // addEventListener是全局函数，为什么不需要import就可以直接使用，是因为window.xxx对象都是不需要import就可以直接使用的全局变量或者函数
 }
 
-export {}
+export {
+    elAutoScaleByWindow,
+}

56cb2dec03e2e5faf7719356ed02fc8e907126de
kaclok <572005238@qq.com>
2024-12-17 08:40:17 +0800

    HttpStatusCode

--- frontend/dailypaper/src/framework/services/net/HttpCodeMap.js
+++ frontend/dailypaper/src/framework/services/net/HttpCodeMap.js
@@ -1,9 +1,14 @@
 // https://www.bilibili.com/video/BV1DKDMYBETU?spm_id_from=333.788.videopod.sections&vd_source=5c9f5bd891aee351c325bcf632b5550f
+import {HttpStatusCode} from "axios";
+
 const httpCodeMap = {
-    404: (response) => {
+    [HttpStatusCode.BadRequest]: (response) => {
 
     },
-    403: (response) => {
+    [HttpStatusCode.Forbidden]: (response) => {
+
+    },
+    [HttpStatusCode.NotFound]: (response) => {
 
     },
 }
